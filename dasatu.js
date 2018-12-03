64bb347e8dc3215c82f7e1eddb8c1df85931fdf1
minmaneh <43015690+minmaneh@users.noreply.github.com>
2018-12-03 14:19:20 +0400

    Add files via upload

---
+++ dasatu.js
@@ -0,0 +1,83 @@
+const FluffyAmericanPancakes=["Fluffy American Pancakes","https://www.bbc.com/food/recipes/fluffyamericanpancak_74828", "flour", "baking", "powder", "egg", "milk", "sugar","salt","butter"];
+const BananaOatWaffles=["Banana Oat Waffles","https://www.kimscravings.com/gluten-free-banana-oat-waffles/","oats", "milk", "vanilla", "extract", "eggs","banana", "cinnamon", "baking", "powder","sweetener","optional toppings"];
+const EggsBenedictToastWithAvocado = ["Eggs Benedict Toeast With Avocado","https://thestayathomechef.com/eggs-benedict-la-avocado-toast-with/", "egg", "vinegar", "bread","olive", "oil", "avocado","red","pepper", "lime salt"];
+const AppleCrumble=["Apple Crumble","https://amyshealthybaking.com/blog/2016/10/02/the-ultimate-healthy-apple-crumble/", "oats", "whole wheat", "flour", "cinnamon", "maple", "syrup", "butter", "red", "apple", "cornstarch", "cinnamon" , "nuts", "nut"];
+const CottageCheeseCasserole = ["Cottage Chesse Casserole","http://recipes777.info/cottage-cheese-casserole-2/","cottage", "cheese", "egg", "vanilla", "sugar", "butter"];
+const SpinachAndCheeseOmlet = ["Spinach and Cheese Omlet","https://www.myrecipes.com/recipe/spinach-and-cheese-omelet","eggs", "butter", "spinach", "tomatoes", "salt", "cheese","pepper"];
+const OatmealPorridgeWithDryFruits= ["Oatmeal Porridge With Dry Fruits","http://www.readersdigest.com.au/recipes/oatmeal-porridge", "oat","oats", "pistachios","milk", "raisin", "apricot", "cranberry", "sugar", "cinnamon"];
+const YogurtAndFruitParfait = ["Yogurt and Fruit Parfait","https://www.foodnetwork.com/recipes/rachael-ray/yogurt-and-fruit-parfaits-recipe-1940127", "yogurt","blackberry", "raspberry", "blueberry", "granola", "strawberry"];
+const BreakfastCookies = ["Breakfast Cookies","https://chocolatecoveredkatie.com/2018/01/18/breakfast-cookies-recipe/","oat", "oats", "applesauce", "mashed", " banana", "almond", "butter", "vanilla", "extract", "salt", "chocalte", "chips", "raspberries", "dried", "fruit", "sugar", "chia", "seeds","coconut"];
+const FrenchToastRecipes = ["French Toast Recipes","https://www.mccormick.com/recipes/breakfast-brunch/quick-and-easy-french-toast","egg", "vanilla", "extract", "cinnamon", "milk", "bread"];
+const HardBoiledEggs =["Hard Boiled Eggs","https://www.yummly.com/recipe/Hard-Boiled-Eggs-Benedict-1546285","eggs", "bread", "bacon", "parsley", "lemon", "butter", "salt", "pepper"];
+const EasyBreakfastMuffins =["Easy Breakfast Muffins","https://cleanfoodcrush.com/three-ingredient-banana-egg-muffins/", "banana", "egg", "berry"];
+const PeanutButterCookies =["Peanut Butter Cookies","https://www.biggerbolderbaking.com/3-ingredient-peanut-butter-cookies/","sugar", "egg", "peanut", "butter"];
+const BananaBread = ["Banana Bread","https://cookieandkate.com/2015/healthy-banana-bread-recipe/","oil", "honey", "vanilla", "salt", "cinnamon", "flour", "nuts", "milk"];
+const CloudEggs = ["Cloud Eggs","https://www.delish.com/cooking/recipe-ideas/recipes/a52748/cloud-eggs-recipe/","egg", "cheese", "salt", "ham", "pepper", "chives"];
+const RaspberrySwirlRolls = ["Raspberry Swirl Rolls", "https://www.goodhousekeeping.com/food-recipes/a19866394/raspberry-swirl-rolls-recipe/","milk", "cream", "sour", "sugar", "vanilla", "yeast", "flour", "butter", "salt", "oil", "jam", "lemon", "raspberry", "raspberries"];
+const SpinachandCheese= ["Spinach and Cheese","https://www.goodhousekeeping.com/food-recipes/easy/a47537/spinach-and-cheese-breakfast-pockets-recipe/","eggs", "cheese", "spinach", "basil", "tomatoe","tomatoes", "pepper", "salt", "crust"];
+const AppleCinnamonPullApartBread= ["Apple Cinnamon Pull-Apart Bread","https://www.goodhousekeeping.com/food-recipes/a12171/apple-cinnamon-pull-apart-bread-recipe-wdy0315/", "flour", "sugar", "dry", "yeast", "salt", "milk", "butter", "egg", "vanilla", "brown", "sugar", "cinnamon", "nutmeg", "apple", "caramel", "sauce" ];
+const FullEnglishBreakfast =["Full English Breakfast","https://www.bbc.com/food/recipes/stressfreefullenglis_67721","sausages", "bacon","mushrooms", "tomatoe", "black", "pudding", "egg", "bread"];
+const PumpkinPieBreakfastBars=["Pumpkin Pie Breakfast Bars","https://ohmyveggies.com/pumpkin-pie-breakfast-bars/","granola", "oil", "pumpkin", "egg", "eggs", "milk", "yogurt", "yoghurt", "maple", "syrup", "pumpkin", "pie", "spice", "vanilla", "salt"];
+const FigAndYogurtBowls=["Fig and Yogurt Bowls","https://www.jamieoliver.com/recipes/fruit-recipes/sticky-cinnamon-fig-yoghurt-breakfast-bowls/","figs","fig", "cinnamon", "orange","honey", "blackberry","yogurt", "yoghurt", "granola", "nuts"] 
+const BreakfastDoughnuts = ["Breakfast Doughnuts","https://www.jamieoliver.com/recipes/breakfast-recipes/breakfast-doughnuts/","dates", "flour", "almonds", "egg", "oil", "blueberry", "honey","yogurt", "yoghurt", "cinnamon"];
+const FrenchRolledUpToasts =["French Rolled-Up Toasts","https://www.foodnetwork.com/recipes/food-network-kitchen/french-toast-roll-ups-4623213","egg", "eggs", "milk", "sugar", "vanilla", "extract", "cinnamon", "salt", "bread", "unsalted", "butter", "maple", "syrup", "strawberry", "banana"];
+const QuinoaBowl = ["Quinoa Bowl","https://www.tastemade.com/videos/breakfast-quinoa-bowl-with-fried-egg","bacon", "onion", "quinoa", "arugula", "olive", "oil", "eggs", "egg"];
+const ChoclateAndCoconutCups=["Choclate and Coconut Cups","https://www.tastemade.com/videos/chocolate-and-coconut-cup","condensed", "milk", "butter", "coconut", "choclate", "cream"];
+const Crepes =["Crepes","https://www.allrecipes.com/recipe/16383/basic-crepes/","butter", "salt", "water", "milk", "egg", "eggs", "flour"];
+const SalmonAndAvocadoToast = ["Salmon And Avocado Toeast","https://www.azestybite.com/salmon-avocado-toast/", "bread", "cheese", "avocado", "salmon", "onion", "capers"];
+const BreakfastBrownies = ["Breakfast Brownies","https://chocolatecoveredkatie.com/2016/06/06/breakfast-bars-chocolate-brownie/","oat", "oats", "sugar", "flax-seed", "flour", "cinnamon", "baking" , "powder", "egg", "milk", "salt", "banana", "vanilla", "extract"];
+const StrawberryProteinSmoothie=["Strawberry Protein Smoothie","https://jolynneshane.com/roasted-strawberry-smoothie-recipe.html", "strawberry", "strawberries", "cottage", "cheese", "honey", "vanilla", "water", "ice", "cream"];
+const ButtermilkCheeseScones =["Buttermilk Cheese Scones","https://www.realsimple.com/food-recipes/browse-all-recipes/buttermilk-cheese-scones", "flour", "baking", "powder", "butter", "cheese", "parmesan", "thyme", "pepper", "buttermilk"];
+const SoftBagels= ["Soft Bagels","http://www.sophisticatedgourmet.com/2009/10/new-york-style-bagel-recipe/","yeast", "sugar", "water", "flour", "salt"];
+
+const BreakfastIdeas = [FluffyAmericanPancakes, BananaOatWaffles, EggsBenedictToastWithAvocado, AppleCrumble, CottageCheeseCasserole, SpinachAndCheeseOmlet, OatmealPorridgeWithDryFruits,  YogurtAndFruitParfait, BreakfastCookies, FrenchToastRecipes, HardBoiledEggs,EasyBreakfastMuffins, PeanutButterCookies, BananaBread, CloudEggs, RaspberrySwirlRolls, SpinachandCheese,  AppleCinnamonPullApartBread,  FullEnglishBreakfast, PumpkinPieBreakfastBars, FigAndYogurtBowls, BreakfastDoughnuts, FrenchRolledUpToasts, QuinoaBowl, ChoclateAndCoconutCups, Crepes, SalmonAndAvocadoToast, BreakfastBrownies, ButtermilkCheeseScones, SoftBagels];
+
+const input=document.getElementById("search");
+
+input.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+     forceLower(document.getElementById("search").value);
+    
+  }
+});
+function forceLower(ingredient){
+    FindBreakfast(ingredient.toLowerCase());
+    }
+
+const FindBreakfast= function (ingredient) {
+    let matches =[];
+    for(let i=0; i<BreakfastIdeas.length; i++){
+      const currentResult = FindMatches(BreakfastIdeas[i], ingredient);
+      if (currentResult) {
+        matches.push(currentResult);
+      }
+    }
+    console.log(matches)
+    const linksHtml = Hyperlinks(matches);
+    $('#popup').html(linksHtml);
+    $('#open').click();
+};
+const FindMatches = function (breakfastIdea, ingredient){
+    for(let i=0; i<breakfastIdea.length; i++){
+      if (breakfastIdea[i]===ingredient){
+         return {
+             title: breakfastIdea[0],
+             link: breakfastIdea[1]
+         }; 
+      }
+    }
+    return false;
+};
+
+const Hyperlinks= function(matches){
+    let linksHtml = '';
+    for(let i=0; i<matches.length; i++){
+        linksHtml+="<a href='"+matches[i].link+"'>"+matches[i].title+"</a><br />";
+    }
+
+   return linksHtml; 
+};
